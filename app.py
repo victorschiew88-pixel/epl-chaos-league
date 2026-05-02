3a6cb21bc4dbe596763e0bd23fda4c0846274aa9
victorschiew88-pixel <victorschiew88@gmail.com>
2026-05-02 17:20:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ hide_st_style = """
 
             /* The Main Box: Using % instead of px for responsiveness */
             .stMainBlockContainer {
-                max-width: 750% !important; /* Expansion on large screens */
+                max-width: 75% !important; /* Expansion on large screens */
                 background-color: rgba(0, 0, 0, 0.3) !important; /* Your preferred transparency */
                 padding: 20px !important;
                 border-radius: 20px;
